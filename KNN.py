025606c50a9516e74be1b10cd1396103b49d6520
Shawww0327 <63212676+Shawww0327@users.noreply.github.com>
2020-04-20 01:26:16 +0100

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,48 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+# 计算测试数据与每一个训练数据的距离
+def distance(train_x, test_x, i):
+    all_dist = np.sqrt(np.sum(np.square(test_x[i] - train_x), axis=1)).tolist()
+    return all_dist
+
+# 查找最近的K个数据所对应的预测值
+def get_number(all_dist):
+    all_number = []
+    for i in range(10):
+        min_index = np.argmin(all_dist)
+        number = np.argmax((train_y[min_index])).tolist()
+        all_number.append(number)
+        #在距离数组中，将最小的距离值删去
+        min_number = min(all_dist)
+        min_number_index = all_dist.index(min_number)
+        del all_dist[min_number_index]
+    return all_number
+
+# 通过众数找到频率最多的类
+def get_most_number(all_number):
+    new_number = np.array(all_number)
+    counts = np.bincount(new_number)
+    return np.argmax(counts)
+
+def knn(train_x, test_x, test_y):
+    predict_y = []
+    for i in range(len(test_x)):
+        all_dist = distance(train_x, test_x, i)
+        all_number = get_number(all_dist)
+        min_number = get_most_number(all_number)
+        predict_y.append(min_number)
+    print("KNN决策树准确率: %.4lf" % accuracy_score(predict_y, test_y))
+
+if __name__ == '__main__':
+    digits = load_digits()
+    data = digits.data
+    train_x, test_x, train_y, test_y = train_test_split(data, digits.target, test_size=0.25, random_state=30)
+    ss = preprocessing.StandardScaler()
+    train_xs = ss.fit_transform(train_x)
+    test_xs = ss.transform(test_x)
+    knn(train_xs, test_xs, test_y)
---
+++ naive_bayesian.py
@@ -0,0 +1,45 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+def prob(train_x, train_y):
+    # 先验分布数组
+    prior = np.zeros(10)
+    # 条件分布矩阵
+    conditional_sum = np.zeros((10, 64))  # condition probability
+    for i in range(1347):
+        prior[train_y[i]] = prior[train_y[i]] + 1
+        for j in range(64):
+            conditional_sum[train_y[i]][j] = conditional_sum[train_y[i]][j] + train_x[i][j]
+    # 先验概率和条件概率
+    prior_prob = (prior + 1) / 1357
+    conditional_prob = (conditional_sum.T + 1) / (prior + 2)
+    return prior_prob, conditional_prob
+
+def naive_bayesian(test_xm, conditional_one_prob_log, conditional_zero_prob_log):
+    predict = np.zeros(450)
+    for m in range(450):
+        for i in range(10):
+            predict_prob = np.zeros(10)
+            for j in range(64):
+                predict_prob[i] += conditional_one_prob_log[j][i] if test_xm[m][j] > 0 else conditional_zero_prob_log[j][i]
+        predict[m] = np.argmax(predict_prob)
+    return predict
+
+
+if __name__ == '__main__':
+    digits = load_digits()
+    data = digits.data
+    train_x, test_x, train_y, test_y = train_test_split(data, digits.target, test_size=0.25, random_state=30)
+    train_xm = (train_x > 0).astype('uint8')
+    test_xm = (test_x > 0).astype('uint8')
+    train_xm = np.reshape(train_xm, (1347, -1))
+    test_xm = np.reshape(test_xm, (450, -1))
+    prior_prob, conditional_prob = prob(train_xm, train_y)
+    conditional_one_prob_log = np.log(conditional_prob)
+    conditional_zero_prob_log = np.log(1 - conditional_prob)
+    predict_y = naive_bayesian(test_xm, conditional_one_prob_log, conditional_zero_prob_log)
+
+    print("朴素贝叶斯分类器准确率: %.4lf" % accuracy_score(predict_y, test_y))
